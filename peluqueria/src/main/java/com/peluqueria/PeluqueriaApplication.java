2b599f6b6db13b87b06de348fbcf045d2b756fd7
Santi03fs <santifernandez652@gmail.com>
2026-06-02 13:11:03 +0200

    todoo ready
    todoo ready

--- peluqueria/src/main/java/com/peluqueria/PeluqueriaApplication.java
+++ peluqueria/src/main/java/com/peluqueria/PeluqueriaApplication.java
@@ -2,9 +2,16 @@ package com.peluqueria;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class PeluqueriaApplication {
+public class PeluqueriaApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(PeluqueriaApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(PeluqueriaApplication.class, args);
@@ -12,10 +19,7 @@ public class PeluqueriaApplication {
 
 }
 
-//16mb
-//SET GLOBAL max_allowed_packet = 16777216;
+// 16mb
+// SET GLOBAL max_allowed_packet = 16777216;
 
-
-
-
-//http://localhost:8080/swagger-ui/index.html#/
+// http://localhost:8080/swagger-ui/index.html#/
